c2cc31766998f25dce116ff6b08248a83c646bf9
darkshadowrule <prajjwaldxi3@gmail.com>
2019-01-08 22:56:45 +0530

    Add post /user route and Token

--- playground/mongoose-delete.js
+++ playground/mongoose-delete.js
@@ -1,13 +1,20 @@
 const {mongoose}=require('../server/db/mongoose')
 const {Todo}=require('../server/models/todo')
 const{ObjectID}=require('mongodb')
-//
+
+
+//First way
 // Todo.remove({}).then((result)=>{
 //   console.log(result);
 // })
+
+//Second way
 // Todo.findByIdAndRemove("5c2f9a0d1f6125e53d977460").then((res)=>{
 //   console.log(res);
 // })
+
+
+//Third way
 Todo.findOneAndRemove({"do":"nothing"}).then((res)=>{
   console.log(res);
 })
